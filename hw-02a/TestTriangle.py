607eba8cc3451a5a5136645a90786581c6468884
Ashay Pable <apable@stevens.edu>
2023-09-22 19:10:19 -0400

    Run Test on buggy code

--- hw-02a/TestTriangle.py
+++ hw-02a/TestTriangle.py
@@ -24,7 +24,22 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be Equilateral')
+    
+    def testNegativeInput(self):
+        self.assertEqual(classifyTriangle(5,-3,4),'InvalidInput','length cannot be negative')
+
+    def testZeroInput(self):
+        self.assertEqual(classifyTriangle(5,0,4),'InvalidInput','length cannot be zero')
+
+    def testScaleneInputA(self):
+        self.assertEqual(classifyTriangle(5,6,4),'Scalene','5,6,4 is a Scalene triangle')
+
+    def testIsocelesInput(self):
+        self.assertEqual(classifyTriangle(5,4,4),'Isoceles','5,4,4 is an Isoceles triangle')
+
+    def testScaleneInputB(self):
+        self.assertEqual(classifyTriangle(15,6,4),'NotATriangle','15,6,4 is not a triangle')
 
 if __name__ == '__main__':
     print('Running unit tests')
