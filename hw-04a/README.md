04d9e0d3ee28b9e8a007fab4a4c91592152123b5
Ashay Pable <apable@stevens.edu>
2023-10-15 19:25:59 -0400

    Hw-05a changes

--- hw-04a/README.md
+++ hw-04a/README.md
@@ -1,4 +1,4 @@
-[![build status of master](https://circleci.com/gh/Ashayp/SSW567.svg?style=svg)](https://app.circleci.com/pipelines/github/Ashayp/SSW567?branch=main&filter=all)
+[![build status of master](https://circleci.com/gh/Ashayp/SSW567.svg?style=svg)](https://app.circleci.com/pipelines/github/Ashayp/SSW567?branch=HW05a_Mocking&filter=all)
 
 # ssw567-hw04a
 Repository for hw04a
--- hw-04a/Test_gitapis.py
+++ hw-04a/Test_gitapis.py
@@ -1,17 +1,30 @@
 import unittest
+from unittest.mock import patch
 from git_apis import getCommits,getRepos
 
 class TestGit(unittest.TestCase):
-    
-    def test_getCommits(self):
-        result = getCommits("Ashayp", "HelloWorld")
+
+    @patch('git_apis.requests.get')
+    def test_getCommits(self, mocks_req):
+        mocks_req.status_code = 200
+        mocks_req.json.return_value =  [{'commit' : {"message": "Merge branch"}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}, {'commit' : {"message": ""}}]
+        mocks_req.side_effect = [mocks_req]
+        result = getCommits("Ashayp", "HelloWorld")        
         self.assertEqual(result, 8)
 
-    def test_checkgitapistate(self):
-        result = getRepos("Ashayp")
+    @patch('git_apis.requests.get')
+    def test_checkgitapistate(self, mocks_req):        #should not be mocked as it defeats the purpose of this test, but done for the sake of completion
+        mocks_req.status_code = 200
+        mocks_req.json.return_value = [{"name": 'Ashayp'}, {"name": 'HelloWorld'}]
+        mocks_req.side_effect = [mocks_req]
+        result = getRepos("Ashayp")        
         self.assertGreater(len(result), 0)
 
-    def test_getCommits(self):
+    @patch('git_apis.requests.get')
+    def test_getCommits2(self, mocks_req):
+        mocks_req.status_code = 200
+        mocks_req.json.return_value =  []
+        mocks_req.side_effect = [mocks_req]
         result = getCommits("ahsgdkeh", "ahsjdgs")
         self.assertNotEqual(result, 1)
     
